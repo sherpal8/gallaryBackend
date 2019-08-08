4982aad2d1d2739e125ba63e4e42808a1e4f959f
sherpal8 <sherpalkorotana@icloud.com>
2019-08-08 12:42:26 +0100

    changes to deploy local repository to heroku

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { apiRouter } = require("./router/apiRouter");
 
 const {
   errorHandler400,
   errorHandler422,
   errorHandler404,
-  errorHandler500
+  errorHandler500,
+  errorHandler405
 } = require("./errors/errors.js");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -21,5 +25,6 @@ app.use(errorHandler400);
 app.use(errorHandler422);
 app.use(errorHandler404);
 app.use(errorHandler500);
+app.use(errorHandler405);
 
 module.exports = { app };
