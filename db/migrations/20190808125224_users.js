875ef75ec5f5a250273aab8a715227ef75594161
sherpal8 <sherpalkorotana@icloud.com>
2019-08-08 17:13:58 +0100

    changes to migrations file

--- db/migrations/20190808125224_users.js
+++ db/migrations/20190808125224_users.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .unique()
+      .primary()
+      .notNullable();
+    usersTable.string("password");
+    usersTable.string("email");
+    usersTable.string("name");
+    usersTable.string("avatarurl").notNullable();
+  });
 };
